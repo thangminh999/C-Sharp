af64fd210f9d6766348ef99438492b84c61ff6f3
thangminh999 <hoangngocminhthang@gmail.com>
2023-04-16 22:58:06 +0700

    Xong Admin

--- Phát triển ứng dụng Desktop/QLQuanCafe/project/QuanLyQuanCafe/QuanLyQuanCafe/fAccountProfile.cs
+++ Phát triển ứng dụng Desktop/QLQuanCafe/project/QuanLyQuanCafe/QuanLyQuanCafe/fAccountProfile.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace QuanLyQuanCafe
 {
-    public partial class Form1 : Form
+    public partial class frmAccountProfile : Form
     {
-        public Form1()
+        public frmAccountProfile()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
